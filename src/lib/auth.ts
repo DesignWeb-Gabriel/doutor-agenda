7567abbe7da10bea1b2411991297e7316e59b3e4
Gabriel Raiol Rodrigues <gabrielraiolr@gmail.com>
2025-05-30 16:58:45 -0300

    feat: add signup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,23 +1,34 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import { db } from "./database.js";
+import * as schema from "@/db/schema";
+
+import { db } from "./database";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "pg" or ysql"
     usePlural: true,
+    schema: {
+      users: schema.usersTable,
+      sessions: schema.sessionsTable,
+      accounts: schema.accountsTable,
+      verificationTokens: schema.verificationsTokensTable,
+    },
   }),
   user: {
-    modelName: "UsersTable",
+    modelName: "users",
   },
   session: {
-    modelName: "SessionsTable",
+    modelName: "sessions",
   },
   account: {
-    modelName: "AccountsTable",
+    modelName: "accounts",
   },
   verificationToken: {
-    modelName: "VerificationsTokensTable",
+    modelName: "verifications_tokens",
+  },
+  emailAndPassword: {
+    enabled: true,
   },
 });
